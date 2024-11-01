2c2d139a7e5ab1b04024da4c588a3de1bc110a01
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-11-01 18:44:23 -0300

    exercicio aula 5 - módulo 2: criar funções com retorno e armazenar em variáveis

---
+++ Exercicios/assets/script.js
@@ -0,0 +1,9 @@
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+    console.log('Testando 1,2,3');
+}
+
+
+let completo = nomeCompleto("Caio", "Henrique");
+
+console.log(completo);
