42448727efbc4400a04d3dd472e6a4e17dcc89bc
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-11-01 20:15:09 -0300

    propósito do exercicio

--- Exercicios/assets/script.js
+++ Exercicios/assets/script.js
@@ -1,8 +1,8 @@
-function calculaPorcentagem(n1, n2) {
-    return (n2 / n1) * 100;
-}
-
-let x = 202;
-let y = 41;
-let pct = calculaPorcentagem(x, y);
-console.log(`${pct}% de ${x} é ${y}.`)
+/*
+Calcule o pre;o do imóvel
+- m2 = 3.000
+- Se tiver 1 quarto, o m2 é 1x
+- Se tiver 2 quartos, o m2 é 1.2x
+- Se tiver 3 quartos, o m2 é 1.5x
+Uso da função:
+*/
