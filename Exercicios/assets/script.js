ffc426b8d6c1d0363d2c1a07ed26d6ce3c580266
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-11-04 11:29:32 -0300

    exemplo: como criar e organizar arrays

--- Exercicios/assets/script.js
+++ Exercicios/assets/script.js
@@ -1,19 +1,22 @@
 // Função padrão para ver o conteúdo das variáveis dentro da página e não apenas no console.
 
-function addTextDOM(text) {
-    const elementContent = document.querySelector('#content');
+// function addTextDOM(text) {
+//     const elementContent = document.querySelector('#content');
 
-    const paragraph = document.createElement('p');
+//     const paragraph = document.createElement('p');
 
-    paragraph.textContent = text;
+//     paragraph.textContent = text;
 
-    elementContent.appendChild(paragraph);
-}
+//     elementContent.appendChild(paragraph);
+// }
 
 // O exemplo das aulas começa à partir daqui:
 
-let colors = ['blue', 'red', 'green', 'yellow'];
 
-addTextDOM(colors);
+let lista = [
+    'Caio', 
+    'Lucas', 
+    [ 'João', 'Pedro' ]
+];
 
-console.log(colors);
+console.log(lista[2][0]);
