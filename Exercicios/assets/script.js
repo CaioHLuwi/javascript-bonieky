9e138aa050b798c747344a9544c5a025cb5a78d9
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-11-04 21:47:17 -0300

    exemplo: formas de usar o laço de repetição e loops - for

--- Exercicios/assets/script.js
+++ Exercicios/assets/script.js
@@ -1,11 +1,23 @@
-let pessoa = {
-    nome: 'Caio',
-    sobrenome: 'Henrique',
-    idade: 20,
-    nomeCompleto: function () {
-        return `${this.nome} ${this.sobrenome}`;
-    }
+let cores = [
+    {nome: 'Preto', qt: 10},
+    {nome: 'Laranja', qt: 5},
+    {nome: 'Branco', qt: 15},
+    {nome: 'Cinza', qt: 20}
+]
 
+for(let n = 0; n < cores.length ; n++) {
+    console.log(`${cores[n].nome} - ${cores[n].qt}`);
 }
 
-console.log(pessoa.nomeCompleto());
+
+/*
+for(let i in cores){
+    console.log(`${cores[i].nome} - ${cores[i].qt}`);
+}
+*/
+
+/*
+for (let cor of cores){
+    console.log(`Nome ${cor.nome} - ${cor.qt}.`)
+}
+*/
