114d7ad83aa33a61de8f06903499fe46a3959751
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-11-05 15:46:33 -0300

    alterar

--- Exercicios/assets/script.js
+++ Exercicios/assets/script.js
@@ -5,16 +5,15 @@ let cores = [
     {nome: 'Cinza', qt: 20}
 ]
 
+/*
 for(let n = 0; n < cores.length ; n++) {
     console.log(`${cores[n].nome} - ${cores[n].qt}`);
 }
+*/
 
-
-/*
 for(let i in cores){
     console.log(`${cores[i].nome} - ${cores[i].qt}`);
 }
-*/
 
 /*
 for (let cor of cores){
